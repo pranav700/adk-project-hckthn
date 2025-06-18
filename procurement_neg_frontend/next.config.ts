47961e6dbfa389a59af3aa7293592ae8daa5d294
Pranav K <pranav_16@outlook.com>
2025-06-18 21:04:34 +0100

    updated

--- procurement_neg_frontend/next.config.ts
+++ procurement_neg_frontend/next.config.ts
@@ -5,6 +5,9 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  }
 };
 
 export default nextConfig;
